81bf9999aec96b49044f432688a20c2581c03c64
rizkimul <riizkimaul4@gmail.com>
2022-01-11 11:44:50 +0700

    done diagram tapi belum fix & fix tombol laporan untuk role superadmin, admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,6 +16,12 @@ class DashboardController extends Controller
         $data = Rental::where('status', 'Accept')->get()->groupBy(function($data){
             return Carbon::parse($data->tanggal_akhir)->format('M');
         });
+        $ruangan = Rental::where('status', 'Accept')->with('room')->get()->groupBy(function($ruangan){
+            return $ruangan->room->roomname;
+        });
+        $user = Rental::where('status', 'Accept')->with('user')->get()->groupBy(function($user){
+            return $user->user->username;
+        });
 
         $months=[];
         $monthCount=[];
@@ -23,6 +29,23 @@ class DashboardController extends Controller
             $months[]=$month;
             $monthCount[]=count($values);
         }
+
+        $labelNama=[];
+        $jumlah=[];
+        foreach($ruangan as $nama => $nilai){
+            $labelNama[]=$nama;
+            $jumlah[]=count($nilai);
+        }
+
+        $namaUser=[];
+        $jumlahPinjam=[];
+        foreach($user as $userName => $value){
+            $namaUser[]=$userName;
+            $jumlahPinjam[]=count($value);
+        }
+
+
+
         return view('gedung.dashboard', [
             'title' => 'Dashboard',
             'user' => User::count(),
@@ -31,7 +54,13 @@ class DashboardController extends Controller
             'pinjam' => Rental::count(),
             'data' => $data,
             'months' => $months,
-            'monthCount' => $monthCount
+            'monthCount' => $monthCount,
+            'ruang' => $ruangan,
+            'nama' => $labelNama,
+            'jumlah' => $jumlah,
+            'pengguna' => $user,
+            'namaUser' => $namaUser,
+            'jumlahPinjam' => $jumlahPinjam
         ]);
         // return view('dashboard.calendar', [
         //     'title' => 'Dashboard'
