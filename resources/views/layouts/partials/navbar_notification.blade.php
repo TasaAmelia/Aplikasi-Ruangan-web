6040aaf9b21142ceeab9ed5bd26bff633f28c88a
rizkimul <riizkimaul4@gmail.com>
2022-02-17 20:18:57 +0700

    done notifikasi

--- resources/views/layouts/partials/navbar_notification.blade.php
+++ resources/views/layouts/partials/navbar_notification.blade.php
@@ -1,18 +1,32 @@
 <div class="dropdown-menu dropdown-list dropdown-menu-right">
     <div class="dropdown-header">Notifications
         <div class="float-right">
-        <a href="#" onclick="abc()">Mark All As Read</a>
+        <a href="#" onclick="markasread()">Mark All As Read</a>
     </div>
     </div>
     <div class="dropdown-list-content dropdown-list-icons">
         @foreach (auth()->user()->unreadNotifications as $notification)
         <div class="dropdown-item dropdown-item-unread">
-            <div class="dropdown-item-icon bg-primary text-white">
-                <i class="fas fa-code"></i>
+            <div class="dropdown-item-icon bg-danger text-white">
+                <i class="fas fa-envelope"></i>
             </div>
             <div class="dropdown-item-desc">
-                {{ $notification->data['ruangan'] }} Dengan Peminjam {{ $notification->data['user'] }} Memiliki Status {{ $notification->data['message'] }}
+               <strong> {{ $notification->data['ruangan'] }} Dengan Peminjam {{ $notification->data['user'] }} Memiliki Status {{ $notification->data['message'] }} </strong>
             </div>
         </div>
         @endforeach
+        @foreach (auth()->user()->readNotifications as $notif)
+        <div class="dropdown-item">
+            <div class="dropdown-item-icon bg-primary text-white">
+                <i class="fas fa-envelope-open"></i>
+            </div>
+            <div class="dropdown-item-desc">
+               {{ $notif->data['ruangan'] }} Dengan Peminjam {{ $notif->data['user'] }} Memiliki Status {{ $notif->data['message'] }}
+            </div>
+        </div>
+        @endforeach
+    </div>
+    <div class="dropdown-footer text-center">
+        <a href="#">View All <i class="fas fa-chevron-right"></i></a>
+    </div>
 </div>
